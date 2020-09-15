d3aad52d80569dcee2e68c048c5161d21e6c1571
Hubert Mielnik <34644791+HM-GDEV@users.noreply.github.com>
2020-09-15 13:38:27 +0200

    A basic console for text-based game

---
+++ Console.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Console : MonoBehaviour
+{
+    public static Console Instance;
+    public GameObject player;
+    public InputAction[] inputActions;
+    public InputField cmdHistory;
+    public InputField cmdField;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            ExecuteCommand();
+        }
+    }
+
+    public void ExecuteCommand()
+    {
+        if (cmdField.text == null)
+        {
+            cmdField.ActivateInputField();
+            return;
+        }
+
+        string userInput = cmdField.text.ToLower().ToString();
+        cmdHistory.text = userInput + "\n" + cmdHistory.text;
+        char[] delimiterCharacters = {' '};
+        string[] separatedInput = userInput.Split(delimiterCharacters);
+
+        for(int i = 0; i < inputActions.Length; i++)
+        {
+            InputAction action = inputActions[i];
+            if(action.keyWord == separatedInput[0])
+            {
+                action.InputCommand(separatedInput, player);
+            }
+        }
+
+        cmdField.text = null;
+        cmdField.ActivateInputField();
+    }
+}
